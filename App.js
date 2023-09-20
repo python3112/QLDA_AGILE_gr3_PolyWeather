40b5effe48b87acd9a12dd6bcd6ac36e7e696a6a
chudinhhieu <chudinhhieuytty@gmail.com>
2023-09-20 17:58:04 +0700

    hieu code splash_scren

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './All/views/Login_user';
+import Splash_screen from './All/views/Splash_screen';
 
 
 const Stack = createNativeStackNavigator();
@@ -10,7 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name = 'Đăng nhập' component={LoginScreen} options={{headerShown:false}}  />
+        <Stack.Screen name = 'Màn hình chào' component={Splash_screen} options={{headerShown:false}}  />
+        <Stack.Screen name = 'Đăng nhập' component={LoginScreen} options={{headerShown:false}}  />
         </Stack.Navigator>
 
     </NavigationContainer>
