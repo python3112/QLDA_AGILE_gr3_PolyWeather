0bc5a145e1082f470f44909f15fe2223ef4a19c8
chudinhhieu <chudinhhieuytty@gmail.com>
2023-09-22 23:43:43 +0700

    update ui

--- All/views/Login_user.js
+++ All/views/Login_user.js
@@ -1,215 +1,242 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, TouchableOpacity, View, Text, Image, TextInput, ImageBackground } from "react-native";
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import Entypo from 'react-native-vector-icons/Entypo';
-import {useNavigation} from '@react-navigation/native'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  TextInput,
+} from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Entypo from "react-native-vector-icons/Entypo";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = (props) => {
-    const navigation = useNavigation();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPass, setShowPass] = useState(false);
+  const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPass, setShowPass] = useState(false);
 
+  return (
+    <View style={styles.container}>
+      {/* Phần logo */}
+      <View style={styles.logo}>
+        <Image style={styles.imageLogo} source={require("../image/logo.png")} />
+        <Text style={styles.textLogo}>POLY WEATHER</Text>
+      </View>
 
-
-    return (
-        <ImageBackground source={require('../image/imgBack.jpg')} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <View style={styles.container}>
-
-                <Image style={{ width: 150, height: 150 }} source={require('../image/logoWeather.png')} />
-                <Text style={{fontSize:25, marginBottom:10,color:'white'  , letterSpacing:0.5,fontWeight:'bold'}}>
-                    Đăng Nhập Poly Weather
-                </Text>
-                {/* /////////////// phần text input đăng nhập vào /////////// */}
-                <View style={styles.viewTip} >
-                    <View style={styles.tipUserNameContainer}>
-                        <FontAwesome5 name="user-alt" size={25} color={'rgba(255 , 255 , 255 , 0.7)'} style={{ marginEnd: 10 }} />
-                        <TextInput style={styles.tipUserName} placeholder="nhập user name" onChangeText={(text) => setUsername(text)} />
-                    </View>
-
-                    <View style={styles.tipUserNameContainer}>
-                        <FontAwesome5 name="lock" size={23} color={'rgba(255 , 255 , 255 , 0.5)'} style={{ marginEnd: 10 }} />
-                        <TextInput style={styles.tipPassword} placeholder="nhập user name"
-                            onChangeText={(text) => setPassword(text)}
-                            secureTextEntry={showPass ? false : true} />
-                        <TouchableOpacity onPress={() => setShowPass(!showPass)} style={{ justifyContent: 'center', width: "10%", height: "100%" }}>
-                            <Entypo name={showPass ? "eye" : "eye-with-line"} size={23} color={'rgba(77 , 77 , 255 , 0.6)'} />
-                        </TouchableOpacity>
-                    </View>
-
-
-                </View>
-                <View style={styles.Signup_and_Forgot} >
-                    <TouchableOpacity style={{ justifyContent: 'center' , marginEnd:50 }}>
-                        <Text style={styles.text}>
-                           Đăng kí Account
-                        </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{ justifyContent: 'center'  , marginStart:50 }}>
-                        <Text style={styles.text}>
-                           Quên mật khẩu?
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-
-                {/* /////////////// btn login ///////////////// */}
-
-                <TouchableOpacity style={styles.btn_Login} onPress={() => {navigation.navigate('signup')}} >
-                    <Text style={{
-                        color: "white",
-                        alignSelf: 'center',
-                        fontSize: 17,
-                    }}>
-                        Đăng Nhập
-                    </Text>
-                </TouchableOpacity>
-                {/* //////////////// đăng nhập với google hoặc phở bò  ///////////// */}
-                <View style={styles.loginWithOtherAccount}>
-                    <View style={styles.lineBlack}>
-                        <View style={{ height: 2, backgroundColor: 'white', width: 100, marginTop: 20, marginEnd: 10 }} />
-                        <Text style={{ fontSize: 20, color: 'white' }}>Or</Text>
-                        <View style={{ height: 2, backgroundColor: 'white', width: 100, marginTop: 20, marginStart: 10 }} />
-
-
-                    </View>
-                    {/* ////////////////// logo đăng nhập bằng google hoặc phở bò /////////////// */}
-                    <View style={styles.logoLogin}>
-                        <TouchableOpacity style={styles.logo_Other_Login}>
-                            <Image style={{ width: 50, height: 50 }} source={require('../image/logoFB.png')} />
-
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.logo_Other_Login}>
-                            <Image style={{ width: 50, height: 50 }} source={require('../image/logoGG.png')} />
-
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.logo_Other_Login}>
-                            <Image style={{ width: 50, height: 50 }} source={require('../image/logoTT.png')} />
-
-
-                        </TouchableOpacity>
-
-                    </View>
-
-
-                </View>
-
-
-            </View>
-
-        </ImageBackground>
-
-    )
-
-}
+      <View style={styles.body}>
+        {/* Phần nhập dữ liệu */}
+        <View style={styles.containerInput}>
+          {/* Tài khoản */}
+          <View style={styles.viewInPut}>
+            <FontAwesome5
+              name="user"
+              size={20}
+              color="orange"
+              style={{ marginEnd: 10 }}
+            />
+            <TextInput
+              style={styles.tipUserName}
+              placeholder="Username"
+              onChangeText={(text) => setUsername(text)}
+            />
+          </View>
+          {/* Mật khẩu */}
+          <View style={styles.viewInPut}>
+            <FontAwesome5
+              name="unlock-alt"
+              size={20}
+              color="orange"
+              style={{ marginEnd: 10 }}
+            />
+            <TextInput
+              style={styles.tipPassword}
+              placeholder="Password"
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={showPass ? false : true}
+            />
+            <TouchableOpacity
+              onPress={() => setShowPass(!showPass)}
+              style={{ justifyContent: "center", width: "10%", height: "100%" }}
+            >
+              <Entypo
+                name={showPass ? "eye" : "eye-with-line"}
+                size={20}
+                color="orange"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.btnForgot}>
+          <Text style={styles.textForgot}>Forgot password ?</Text>
+        </TouchableOpacity>
+        {/* Button Login => Click => Chuyển sang màn hình Home */}
+        <TouchableOpacity
+          style={styles.btnLogin}
+          onPress={() => {
+            navigation.navigate("signup");
+          }}
+        >
+          <Text style={styles.textBtnLogin}>Login</Text>
+        </TouchableOpacity>
+        
+        {/* Đăng nhặp bằng Facebook và Google */}
+        <View style={styles.loginWithOtherAccount}>
+          <View style={styles.lineBlack}>
+            <View
+              style={{
+                height: 0.5,
+                backgroundColor: "grey",
+                width: '30%',
+              }}
+            />
+            <Text style={{ fontSize: 16, color: "grey",marginHorizontal:15 }}>Or sign in with</Text>
+            <View
+              style={{
+                height: 0.5,
+                backgroundColor: "grey",
+                width: '30%',
+              }}
+            />
+          </View>
+          {/* ////////////////// logo đăng nhập bằng google hoặc phở bò /////////////// */}
+          <View style={styles.logoLogin}>
+            <TouchableOpacity style={styles.logo_Other_Login}>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={require("../image/logoFB.png")}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.logo_Other_Login}>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={require("../image/logoGG.png")}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 export default LoginScreen;
 
-
 const styles = StyleSheet.create({
+  // Chia bố cục
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  logo: {
+    position: "relative",
+    top: 50,
+    alignItems: "center",
+  },
+  body: {
+    width: "93%",
+    position: "relative",
+    top: 80,
+    alignItems: "center",
+  },
+  //Logo
+  imageLogo: {
+    width: 150,
+    height: 150,
+  },
+  textLogo: {
+    marginTop: 10,
+    color: "orange",
+    fontWeight: "400",
+    fontSize: 24,
+  },
 
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+  //  Nhập dữ liệu
+  containerInput: {
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "column",
+  },
 
+  viewInPut: {
+    flexDirection: "row",
+    width: "100%",
+    height: 60,
+    alignItems: "center",
+    paddingStart: 10,
+    marginTop: 15,
+    borderRadius: 10,
+    borderColor: "orange",
+    borderWidth: 1.5,
+  },
+  tipUserName: {
+    width: "80%",
+    maxWidth: 350,
+    paddingStart: 10,
+    color: "black",
+    fontSize: 15,
+  },
+  tipPassword: {
+    width: "82%",
+    maxWidth: 350,
+    paddingStart: 15,
+    color: "black",
+    fontSize: 15,
+  },
 
-    },
-    textLogo: {
-        marginBottom: 20,
-        fontSize: 20,
-        color: 'blue',
+  //Button quên mật khẩu
+  btnForgot: {
+    alignSelf: "flex-end",
+  },
+  textForgot: {
+    marginTop: 10,
+    fontSize: 15,
+    fontWeight: "500",
+    color: "grey",
+  },
 
+  //Button đăng nhập
+  btnLogin: {
+    width: "100%",
+    height: 55,
+    marginTop: 25,
+    borderRadius: 5,
+    backgroundColor: "orange",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textBtnLogin: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 20,
+  },
+  
+//Đăng nhập với Facebook và Google
+  loginWithOtherAccount: {
+    flexDirection: "column",
+    width: "100%",
+    marginTop:25,
+  },
+  lineBlack: {
+    alignItems:'center',
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  logoLogin: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+  },
 
-    },
-
-
-    /////////// phần csss text input ///////////////
-
-
-    viewTip: {
-        width: '100%',
-        alignItems: 'center',
-        flexDirection: 'column',
-
-    },
-
-    tipUserNameContainer: {
-        width: "90%",
-        height: 60,
-        flexDirection: 'row',
-        borderRadius: 10,
-        alignItems: 'center',
-        backgroundColor: 'rgba(255 , 255 , 255 , 0.5)',
-        paddingStart: 10,
-        marginTop: 15,
-    },
-    tipUserName: {
-        width: '78%',
-        maxWidth: 350,
-        paddingStart: 10,
-        color: 'black',
-        fontSize: 15,
-    },
-    tipPassword: {
-        width: '70%',
-        maxWidth: 350,
-        paddingStart: 15,
-        color: 'black',
-        fontSize: 15,
-    },
-    Signup_and_Forgot: {
-        flexDirection: 'row',
-
-        width: "80%",
-        height: 40,
-        justifyContent: "space-evenly",
-        marginTop:15
-
-    },
-    text:{
-        fontSize:17,
-        color:'white',
-    },
-
-    /////////////////// css phần nút ấn và logo ///////////
-
-    btn_Login: {
-        width: 150,
-        borderWidth: 1,
-        padding: 10,
-        borderRadius: 5,
-        marginTop: 10,
-        marginBottom: 10,
-        backgroundColor: "rgba(128,0,128 , 0.8)",
-    },
-
-    loginWithOtherAccount: {
-        flexDirection: 'column',
-        width: "100%",
-        height: 80,
-
-    },
-
-    lineBlack: {
-        marginTop: 10,
-        justifyContent: 'center',
-        flexDirection: 'row'
-    },
-    logoLogin: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginTop: 20,
-
-    },
-
-    logo_Other_Login: {
-        width: 50,
-        height: 50,
-        marginEnd: 20,
-        marginStart: 20,
-        borderRadius: 12,
-        justifyContent: 'center',
-    }
-
-
+  logo_Other_Login: {
+    width: 50,
+    height: 50,
+    marginEnd: 20,
+    marginStart: 20,
+    borderRadius: 12,
+    justifyContent: "center",
+  },
 });
