7be238313b2e298eada020f17cdcd77c1d3443df
chudinhhieu <chudinhhieuytty@gmail.com>
2023-10-02 08:28:05 +0700

    ui

--- All/views/Home_screen.js
+++ All/views/Home_screen.js
@@ -1,126 +1,149 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import React , {useEffect , useState} from 'react'
-import 'react-native-gesture-handler';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import "react-native-gesture-handler";
+import { Appbar } from "react-native-paper";
+import "react-native-dimension";
 
 
 const Home_screen = (props) => {
   const { navigation } = props;
+  const windowWidth = Dimensions.get("window").width * 0.92;
+  const windowWidthDetail = Dimensions.get("window").width * 0.282;
 
- useEffect(() => {
-  AsyncStorage.getItem('Data_User', (error, result) => {
-    if (!error) {
-     const user = JSON.parse(result)
-    
-      console.log('Giá trị đã lưu: ' + result + ' '  );
-    } else {
-      console.log('Lỗi khi đọc giá trị: ' + error);
-    }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "white",
+      width: "100%",
+    },
+    weatherImage: {
+      alignSelf: "center",
+      width: windowWidth,
+      height: windowWidth,
+      borderRadius: 10,
+    },
+    temperatureContainer: {
+      flexDirection: "row",
+      width: "100%",
+      alignItems: "center",
+    },
+    temperatureText: {
+      fontSize: 68,
+      fontWeight: "100",
+      color: "gray",
+      marginEnd: 20,
+    },
+    degreeText: {
+      fontSize: 32,
+      fontWeight: "100",
+      color: "gray",
+    },
+    dateText: {
+      color: "gray",
+    },
+    locationText: {
+      color: "black",
+      fontSize: 28,
+      fontWeight: "bold",
+      marginVertical: 5,
+    },
+    weatherStatusText: {
+      color: "black",
+      fontSize: 16,
+    },
+    detailHeaderText: {
+      color: "black",
+      fontSize: 15,
+      marginTop: 15,
+      fontWeight: "500",
+      marginBottom: 25,
+    },
+    detailBox: {
+      backgroundColor: "#EEEEEE",
+      borderRadius: 5,
+      width: windowWidthDetail,
+      height: windowWidthDetail,
+    },
   });
- }, [])
- 
+
+  const renderDetail = () => {
+    const renderBox = () => {
+      return <View style={styles.detailBox}></View>;
+    };
+
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: windowWidthDetail * 2 + 10,
+          justifyContent: "space-between",
+          flexDirection: "column",
+          marginBottom: 20,
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: 105,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          {renderBox()}
+          {renderBox()}
+          {renderBox()}
+        </View>
+        <View
+          style={{
+            width: "100%",
+            height: 105,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          {renderBox()}
+          {renderBox()}
+          {renderBox()}
+        </View>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <Image source={require("../image/anhHome.jpg")} style={{ width: "100%", height: 300, borderRadius: 10, marginTop: 10 }} />
-      <View style={styles.btnDetails}>
-        <Text style={{ fontSize: 40, color: 'gray' }}>16°C</Text>
-        <Text style={{ color: 'gray', fontSize: 24 }}>18.4°/15°</Text>
-      </View>
-
-      <Text style={{ fontSize: 24 }}>Hà Giang</Text>
-      <Text>Clear shy</Text>
-      <Text>Details</Text>
-      <View style={styles.btnDetails}>
-        <TouchableOpacity style={styles.btn}>
-          <Image style={{ width: 30, height: 30 }} source={require("../image/temperature.png")} />
-          <Text>Feels like</Text>
-          <View style={styles.btnDetails}>
-            <Text style={styles.txtDetail}>16</Text>
-            <Text style={styles.textD}>°C</Text>
-          </View>
-
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
-          <Image style={{ width: 30, height: 30 }} source={require("../image/humidity.png")} />
-          <Text>Humidity</Text>
-          <View style={styles.btnDetails}>
-            <Text style={styles.txtDetail}>59</Text>
-            <Text style={styles.textD}>%</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
-          <Image style={{ width: 30, height: 30 }} source={require("../image/rays.png")} />
-          <Text>Chỉ số UV</Text>
-          <View style={styles.btnDetails}>
-            <Text style={styles.txtDetail}>1</Text>
-          </View>
-
-        </TouchableOpacity>
-      </View>
-      <View style={styles.btnDetails}>
-        <TouchableOpacity style={styles.btn}>
-          <Image style={{ width: 30, height: 30 }} source={require("../image/view.png")} />
-          <Text>Visibility</Text>
-          <View style={styles.btnDetails}>
-            <Text style={styles.txtDetail}>10</Text>
-            <Text style={styles.textD}>Km</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
-          <Image style={{ width: 30, height: 30 }} source={require("../image/wind.png")} />
-          <Text>Wind speed</Text>
-
-          <View style={styles.btnDetails}>
-            <Text style={styles.txtDetail}>10</Text>
-            <Text style={styles.textD}> Km/s</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn}>
-          <Image style={{ width: 30, height: 30 }} source={require("../image/blood-pressure.png")} />
-          <Text>Pressure</Text>
-          <View style={styles.btnDetails}>
-            <Text style={styles.txtDetail}>1015</Text>
-          </View>
-
-        </TouchableOpacity>
-      </View>
-
-
-
+      <ScrollView style={{ width: "100%", paddingHorizontal: 16 }}>
+        {/* Ảnh thời tiết */}
+        <Image
+          style={styles.weatherImage}
+          source={require("../image/cloudy.jpg")}
+        />
+        {/* Nhiệt độ */}
+        <View style={styles.temperatureContainer}>
+          <Text style={styles.temperatureText}>26°C</Text>
+          <Text style={styles.degreeText}>29°</Text>
+          <Text style={styles.degreeText}>/</Text>
+          <Text style={styles.degreeText}>24°</Text>
+        </View>
+        {/* Ngày tháng năm hiện tại*/}
+        <Text style={styles.dateText}>THURSDAY, SEPTEMBER 29, 2023</Text>
+        {/* Địa điểm */}
+        <Text style={styles.locationText}>Nam Tu Liem</Text>
+        {/* Trạng thái thời tiết */}
+        <Text style={styles.weatherStatusText}>Fair</Text>
+        {/* Chi tiết */}
+        <Text style={styles.detailHeaderText}>DETAIL</Text>
+        {renderDetail()}
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default Home_screen
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  btn: {
-    width: 120,
-    height: 120,
-    backgroundColor: 'lightgray',
-    alignItems: 'center',
-    padding: 20,
-    borderRadius: 10
-  },
-  btnDetails: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    margin: 10,
-
-  },
-  txtDetail: {
-    fontSize: 20
-  },
-  textD: {
-    fontSize: 20,
-    color: 'gray',
-    marginLeft: 5
-  }
-
-
-})
+export default Home_screen;
